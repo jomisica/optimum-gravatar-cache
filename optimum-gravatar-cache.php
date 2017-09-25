0b9df0696b1e91bbd9a8179552b2d435fdef8e07
José Miguel Silva Caldeira <miguel@ncdc.pt>
2017-09-25 10:52:53 +0100

    Correcting the create table statement

--- optimum-gravatar-cache.php
+++ optimum-gravatar-cache.php
@@ -444,14 +444,14 @@ class OGC
         global $wpdb;
 
         $wpdb->query("CREATE TABLE IF NOT EXISTS `{$this->cacheTableName}` (
-				  `id` int(11) NOT NULL auto_increment,
+				  `id` int(10) UNSIGNED NOT NULL auto_increment,
 				  `email` varchar(255) NOT NULL,
 				  `hash` char(32) NOT NULL,
 				  `optimized` enum('0','1') NOT NULL,
-				  `size` int(5) NOT NULL,
+				  `size` smallint(5) UNSIGNED NOT NULL,
 				  `ext` enum('svg','jpg','png','gif') NOT NULL,
-				  `lastCheck` int(11) NOT NULL,
-				  `lastModified` int(11) NOT NULL,
+				  `lastCheck` int(10) UNSIGNED NOT NULL,
+				  `lastModified` int(10) UNSIGNED NOT NULL,
 				  `def` enum('0','1') NOT NULL,
           PRIMARY KEY (`id`),
           KEY `hash` (`hash`),
